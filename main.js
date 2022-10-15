140f1b1384501fda9fd567d94f024d302e971dd8
Patricio <patricio.pastor07@gmail.com>
2022-10-15 18:43:42 -0300

    Animation Logo

---
+++ main.js
@@ -0,0 +1,12 @@
+const elements = document.getElementsByClassName("words");
+
+
+for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('animationend', function(e) {
+        elements[i].classList.remove('animated');
+    });
+
+    elements[i].addEventListener('mouseover', function(e) {
+        elements[i].classList.add('animated')
+    })
+}
