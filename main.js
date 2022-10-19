222f9a66e85f85340e7df0687b07d239efdabbf9
Patricio <patricio.pastor07@gmail.com>
2022-10-19 01:54:11 -0300

    Responsive design

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 AOS.init();
 
+
 const elements = document.getElementsByClassName("words");
 
 
@@ -12,3 +13,18 @@ for (let i = 0; i < elements.length; i++) {
         elements[i].classList.add('animated')
     })
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+
+hiddenElements.forEach((el) => observer.observe(el))
